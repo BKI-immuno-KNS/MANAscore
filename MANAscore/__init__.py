4a7cc28ba49513f62c688cd9a326d588deaf56b3
BKI-immuno-KNS <168449999+BKI-immuno-KNS@users.noreply.github.com>
2024-11-08 16:22:12 -0500

    Update __init__.py

--- MANAscore/__init__.py
+++ MANAscore/__init__.py
@@ -1,3 +1,13 @@
 # MANAscore/__init__.py
+"""
+MANAscore
+
+A tool for calculating MANAscore.
+
+This module is part of the larger project and is licensed under the terms of the LICENSE
+file found in the root directory of this repository (https://github.com/BKI-immuno-KNS/MANAscore).
+
+(c) 2024, Zhen and contributors
+"""
 
 from .MANAscore import MANAscore
